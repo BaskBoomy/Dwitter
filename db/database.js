d062464618423d228441ac466a229efa812788e9
jack <whdgurtpqms@gmail.com>
2022-08-29 15:23:28 +0900

    save changes

--- db/database.js
+++ db/database.js
@@ -1,13 +1,20 @@
 import SQ from 'sequelize';
 import { config } from '../config.js';
 
-const {host, user, database, password} = config.db;
+const {host, port,user, database, password} = config.db;
 
 //sequelize 연결
 export const sequelize = new SQ.Sequelize(database, user, password, {
     host,
-    dialect: 'mysql',
-    logging:false
+    port,
+    dialect: 'postgres',
+    logging:false,
+    dialectOptions:{
+        ssl:{
+            require:true,
+            rejectUnauthorized:false,
+        }
+    }
 })
 
 // const pool = mysql.createPool({
